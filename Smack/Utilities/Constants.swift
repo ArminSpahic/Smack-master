583c703f3308bd31021dd13022a2b007a1285efa
Armin Spahic <arminspahic@Armins-MacBook-Pro.local>
2018-01-17 23:24:51 +0100

    Register User

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,10 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Succes: Bool) -> ()
+
+// URL CONSTANTS
+let BASE_URL = "https://git.heroku.com/chattychatas.git"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
 
 
 
