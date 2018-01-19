1d7ed776a3a3a0381fd598e8335eca2df6d81458
Armin Spahic <arminspahic@Armins-MacBook-Pro.local>
2018-01-19 12:09:32 +0100

    log in

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Succes: Bool) -> ()
 // URL CONSTANTS
 let BASE_URL = "https://smackchattyas.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -24,5 +25,7 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
+// Headers
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
 
 
