65741bc366da8f0afb569f5a975300c730e66933
Armin Spahic <arminspahic@Armins-MacBook-Pro.local>
2018-01-21 22:22:38 +0100

    Create account

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
     
-    
+     //  Outlets
     @IBOutlet weak var userImg: UIImageView!
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
    
@@ -24,7 +32,7 @@ class CreateAccountVC: UIViewController {
     
 }
     @IBAction func createAccntPressed(_ sender: Any) {
-        
+        guard let name = usernameTxt.text, usernameTxt.text != "" else {return}
             guard let email = emailTxt.text , emailTxt.text != "" else {return}
             guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
             
@@ -32,7 +40,13 @@ class CreateAccountVC: UIViewController {
                 if success {
                     AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                         if success {
-                            print ("logged in user", AuthService.instance.authToken)
+                            AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                                if success {
+                                    print (UserDataService.instance.name, UserDataService.instance.avatarName)
+                                    self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                    
+                                }
+                            })
                         }
                     })
                 }
